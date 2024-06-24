b95511bb58e78bc25b2faf6f5792f6cfff7ba166
jorgeluis-dev <jorgeluis.geek@gmail.com>
2024-06-24 11:15:08 -0300

    starting project soap

---
+++ src/main/java/com/br/devjorge/serviceWebsoap/SOAPwebservice/ProducingASoapWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.br.devjorge.serviceWebsoap.SOAPwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProducingASoapWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProducingASoapWebServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/devjorge/serviceWebsoap/SOAPwebservice/ProducingASoapWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.devjorge.serviceWebsoap.SOAPwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducingASoapWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
